ec41e6fcc997e6ee4a37e7a96ec04ed18627d141
Sudhir Pal <sudhirnandlalpal@gmail.com>
2020-08-19 14:20:33 +0530

    2nd commit

---
+++ playground/callback.js
@@ -0,0 +1,18 @@
+const geocoding=(addresh,callback)=>
+{
+    setTimeout
+    ( 
+        ()=>
+    {
+    const data=
+    {
+        latitude:0,
+        longitude:0
+    }
+
+    callback(data)
+    
+    }
+,2000
+    )
+}
