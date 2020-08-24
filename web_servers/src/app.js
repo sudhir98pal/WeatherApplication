bb47ba45cc74c9c1ceed8652e26e7ecb0c422476
Sudhir Pal <sudhirnandlalpal@gmail.com>
2020-08-25 03:13:23 +0530

    Front end Explored further

--- web_servers/src/app.js
+++ web_servers/src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const hbs = require('hbs')
+
 
 const app = express();
 // statement creates a new express application for you
 
-app.set('view engine','hbs');
+app.set('view engine', 'hbs');
 // setting app's view engine with hbs
 // view engine is case and space sensitive
 // it is use to create express dynamic handlebars templates
@@ -14,50 +16,60 @@ const htmlfilepath = path.join(__dirname, '../public');
 
 
 // we can also can rename views folder as below
-//const viewspath=path.join(__dirname,'../folder_name');
-//app.set('views',viewspath);
+const viewspath = path.join(__dirname, '../templates/views');
+const partialspath = path.join(__dirname, '../templates/partials');
+
+
+app.set('views', viewspath);
+hbs.registerPartials(partialspath);
 
 app.use(express.static(htmlfilepath));
 
 
-app.get('',(req,res)=>
-{
+app.get('', (req, res) => {
     res.render('index',
-    {
-        title:'Weather App',
-        name:'Sudhir Pal'
-    });
+        {
+            title: 'Weather App',
+            name: 'Sudhir Pal'
+        });
     // to use render here
     // use command like this PS D:\2_weather_app\web_servers> node src/app.js
     // that is come out of src :-> this makes views -> index.hbs rendering possible 
 })
 
-app.get('/about',(req,res)=>
-{
-    res.render('about',{
-        title:'About Me',
-        name:'My name Is Sudhir Pal'
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Hi MySelf Sudhir Pal'
     })
 });
-app.get('/help', (req, res) => 
-{
+app.get('/help', (req, res) => {
     res.render('help',
-    {
-        title:'Welcome To Help Page',
-        body:'This Is A Weather Application You Can Search Any Valid Loactions Weather'
+        {
+            title: 'Welcome To Help Page',
+            body: 'This Is A Weather Application You Can Search Any Valid Loactions Weather'
 
 
+        })
+})
+
+app.get('/weather', (req, res) => {
+    res.render('weather', {
+        title: 'Weather',
+        location: 'Jaunpur'
     })
 })
- 
-app.get('/weather', (req, res) => 
+
+// If Nothing Matches this Webpage is called
+
+app.get('*',(req,res)=>
 {
-    res.render('weather',{
-        title:'Weather',
-        location:'Jaunpur'
+    res.render('404',{
+        title:'404 Error',
+        message:'Opps Page Not Found !'
+
     })
 })
-
 app.listen(3000, () => {
     console.log('server is up on port 3000');
 });
